9a67eeea419ea93f44cb0eba9c6bd33c81a4d28d
Sergey Volkov <js2me@outlook.com>
2025-09-15 11:01:41 +0300

    ci: fix build

--- post-build.mjs
+++ post-build.mjs
@@ -1,31 +1,69 @@
-import { postBuildScript, publishScript } from 'js2me-exports-post-build-script';
-import { copyFileSync } from 'node:fs';
-import { resolve } from 'node:path';
+import { postBuildScript, publishScript, getInfoFromChangelog, publishGhRelease } from 'js2me-exports-post-build-script';
+import path from 'path';
 
 postBuildScript({
   buildDir: 'dist',
   rootDir: '.',
   srcDirName: 'src',
-  filesToCopy: ['LICENSE', 'README.md'],
+  filesToCopy: ['LICENSE', 'README.md', 'assets'],
   updateVersion: process.env.PUBLISH_VERSION,
-  onDone: (versionsDiff, _, packageJson, { targetPackageJson }) => {
-    // Копируем bin.ts в bin.mjs в папку dist
-    copyFileSync(
-      resolve('dist/cli', 'bin.js'),
-      resolve('dist/cli', 'bin.mjs')
-    );
-
+  onDone: (versionsDiff, targetPackageJson, { $ }) => {
     if (process.env.PUBLISH) {
-      publishScript({
-        nextVersion: versionsDiff?.next ?? packageJson.version,
-        currVersion: versionsDiff?.current,
+      if (!process.env.CI) {
+        $(`pnpm test`);
+        $('pnpm changeset version');
+      }
+
+      // Копируем bin.ts в bin.mjs в папку dist
+      copyFileSync(
+        resolve('dist/cli', 'bin.js'),
+        resolve('dist/cli', 'bin.mjs')
+      );
+
+
+      // remove all test compiled files. TODO: find a better to ignore test files
+      $('rm dist/**/*.test.*');
+
+      const nextVersion = versionsDiff?.next ?? targetPackageJson.data.version;
+
+      const publishOutput = publishScript({
+        gitTagFormat: '<tag>',
+        nextVersion: nextVersion,
         packageManager: 'pnpm',
         commitAllCurrentChanges: true,
         createTag: true,
-        githubRepoLink: 'https://github.com/js2me/mobx-tanstack-query-api',
-        cleanupCommand: 'pnpm clean', 
+        safe: true,
+        onAlreadyPublishedThisVersion: () => {
+          console.warn(`${nextVersion} already published`);
+        },
+        cleanupCommand: 'pnpm clean',
         targetPackageJson
-      })
+      });
+
+      if (process.env.CI) {
+        if (publishOutput?.publishedGitTag) {
+          const { whatChangesText } = getInfoFromChangelog(
+            nextVersion,
+            path.resolve(targetPackageJson.locationDir, '../CHANGELOG.md'),
+            targetPackageJson.repositoryUrl
+          );
+
+          publishGhRelease({
+            authToken: process.env.GITHUB_TOKEN,
+            body: whatChangesText,
+            owner: targetPackageJson.ghRepoData.user,
+            repo: targetPackageJson.ghRepoData.packageName,
+            version: nextVersion,
+          })
+            .then((r) =>{
+              console.info('published new gh release',r)
+            })
+            .catch((err) =>{
+              console.error('failed to publish new gh release', err);
+              process.exit(1);
+            })
+        }
+      }
     }
   }
 });
